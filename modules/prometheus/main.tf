bd73e6f75f6fd3d4e492a41512d4f8d6dfb470d8
lberes <beres.laszlo1990@gmail.com>
2023-04-02 17:36:04 +0200

    Add prometheus helm chart module

---
+++ modules/prometheus/main.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  namespace  = "kube-monitoring"
+  chart      = "prometheus"
+  version    = "20.0.2"
+  wait       = true
+}
